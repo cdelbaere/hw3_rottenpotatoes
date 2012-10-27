10b368084d7f617d9bc39595f68d1eae86e8a9d9
Christian Delbaere <cdelbaere@rogers.com>
2012-10-26 19:24:07 -0700

    hw3 parts 2 and 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -12,15 +12,31 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  first_position = page.body.index(e1)
+  second_position = page.body.index(e2)
+  first_position.should < second_position
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  rating_list.split(",").each do |rating|
+    rating.strip!
+    if uncheck == "un"
+      step "I uncheck \"ratings[#{rating}]\""
+    else 
+      step "I check \"ratings[#{rating}]\""
+    end
+  end
 end
+
+Then /I should see all of the movies/ do
+  rows = page.all('table#movies tr').count
+  rows.should == 11
+end
+
